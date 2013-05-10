451905dabf4afa4e50c521f39f168aef5e2f0f80
Ahmed Eldawy <eldawy@cs.umn.edu>
2013-05-09 19:24:12 -0500

    Added Apache license to all files
    Import TestHelper from piggybank
    
    Use Pig server in ST_Area test

--- java/src/test/java/edu/umn/cs/spig/test/TestST_Area.java
+++ java/src/test/java/edu/umn/cs/spig/test/TestST_Area.java
@@ -1,12 +1,27 @@
-/**
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the
+ * NOTICE file distributed with this work for additional information regarding copyright ownership. The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
  */
+
 package edu.umn.cs.spig.test;
+
+import static org.apache.pig.ExecType.LOCAL;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import junit.framework.TestCase;
 
+import org.apache.pig.PigServer;
 import org.apache.pig.data.Tuple;
-import org.apache.pig.data.TupleFactory;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
@@ -22,7 +37,9 @@ import edu.umn.cs.spig.ST_Area;
  */
 public class TestST_Area extends TestCase {
   
-  private Geometry polygon;
+  private ArrayList<Geometry> geometries;
+  private ArrayList<String[]> data;
+  
   
   public TestST_Area() {
     GeometryFactory geometryFactory = new GeometryFactory();
@@ -33,15 +50,33 @@ public class TestST_Area extends TestCase {
     coordinates[3] = new Coordinate(10, 0);
     coordinates[4] = new Coordinate(0, 0);
     LinearRing line = geometryFactory.createLinearRing(coordinates);
-    polygon = geometryFactory.createPolygon(line, null);
+    geometries = new ArrayList<Geometry>();
+    geometries.add(geometryFactory.createPolygon(line, null));
+    
+    data = new ArrayList<String[]>();
+    for (int i = 0; i < geometries.size(); i++) {
+      data.add(new String[] {Integer.toString(i), geometries.get(i).toText()});
+    }
   }
-
   
-  public void testShouldWorkWithByteArrayWKT() throws Exception {
-    Tuple tuple = TupleFactory.getInstance().newTuple(1);
-    tuple.set(0, polygon.toString());
-    Double area = new ST_Area().exec(tuple);
-    assertEquals(polygon.getArea(), area);
+  public void testShouldWorkWithWKT() throws Exception {
+    String datafile = TestHelper.createTempFile(data, "\t");
+    datafile = datafile.replace("\\", "\\\\");
+    PigServer pig = new PigServer(LOCAL);
+    String query = "A = LOAD 'file:" + datafile + "' as (id, geom);\n" +
+      "B = FOREACH A GENERATE "+ST_Area.class.getName()+"(geom);";
+    System.out.println(query);
+    pig.registerQuery(query);
+    Iterator<?> it = pig.openIterator("B");
+    Iterator<Geometry> geoms = geometries.iterator();
+    while (it.hasNext() && geoms.hasNext()) {
+      Tuple tuple = (Tuple) it.next();
+      Geometry geom = geoms.next();
+      if (tuple == null)
+        break;
+      Double area = (Double) tuple.get(0);
+      assertEquals(geom.getArea(), area);
+    }
   }
 
 }
